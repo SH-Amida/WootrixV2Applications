2433509335eaf26915e5e6bb4e24aa4c921d5e57
Alex Matthews <30582079+alexmatthewman@users.noreply.github.com>
2019-02-03 12:24:02 -0300

    Adding to cloud
    Adding to cloud

---
+++ WootrixV2Apps/WootrixV2Apps.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.Gms.Common;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace WootrixV2Apps.Droid
+{
+    [Activity(Label = "WootrixV2Apps", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ WootrixV2Apps/WootrixV2Apps/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Android.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WootrixV2Apps
+{
+
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            if (webView.CanGoBack)
+            {
+                webView.GoBack();
+            }
+            else
+            {
+                await Navigation.PopAsync();
+            }
+        }
+
+        void OnForwardButtonClicked(object sender, EventArgs e)
+        {
+            if (webView.CanGoForward)
+            {
+                webView.GoForward();
+            }
+        }
+
+        //protected override bool OnBackButtonPressed()
+        //{
+        //    Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 1]);            
+        //    return false;
+        //    //return base.OnBackButtonPressed();
+        //}
+
+        //protected override bool OnBackButtonPressed()
+        //{
+
+        //    return true;
+        //}
+
+        //public override void OnBackPressed()
+        //{
+        //    if (App.IsRootPage || App.AuthenticationContext == null) return;
+        //    base.OnBackPressed();
+        //}
+    }
+
+}
