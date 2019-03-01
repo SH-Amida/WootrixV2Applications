031b48129cbfeb4128eda217d87e3bdb07080c63
Alex Matthews <30582079+alexmatthewman@users.noreply.github.com>
2019-03-01 13:23:12 -0300

    nOW WITH tokens!

--- WootrixV2Apps/WootrixV2Apps.Android/MainActivity.cs
+++ WootrixV2Apps/WootrixV2Apps.Android/MainActivity.cs
@@ -5,7 +5,6 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using Plugin.PushNotification;
 using Android.Content;
 using Android.Gms.Common;
 using Firebase.Messaging;
@@ -21,8 +20,7 @@ namespace WootrixV2Apps.Droid
     [Activity(Label = "WootrixV2Apps", Icon = "@drawable/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        ///Toolbar toolbar;
-        WootrixV2Apps.MainPage mPage;
+
         const string TAG = "MainActivity.cs";
 
         protected override void OnCreate(Bundle bundle)
@@ -32,85 +30,79 @@ namespace WootrixV2Apps.Droid
 
             //TabLayoutResource = Resource.Layout.Tabbar;
             //ToolbarResource = Resource.Layout.Toolbar;
-            
+
             base.OnCreate(bundle);
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
             System.Diagnostics.Debug.WriteLine($"Check GPS*****************");
             IsPlayServicesAvailable();
             System.Diagnostics.Debug.WriteLine($"Creat Channel*****************");
             CreateNotificationChannel();
+
+
             
             //System.Diagnostics.Debug.WriteLine($"Subscribed To TOPIC: all*****************");
             //FirebaseMessaging.Instance.SubscribeToTopic("all");
-            // PushNotificationManager.ProcessIntent(this, Intent);
-            //
-            //if (Intent.Extras != null)
-            //{
-            //    foreach (var key in Intent.Extras.KeySet())
-            //    {
-            //        var value = Intent.Extras.GetString(key);
-            //        Android.Util.Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
-            //    }
-            //}
+            
         }
 
-        //protected override void OnNewIntent(Intent intent)
-        //{
-        //    Android.Util.Log.Debug(TAG, "Location: OnNewIntent*************");
-        //    base.OnNewIntent(intent);
-        //    PushNotificationManager.ProcessIntent(this, intent);
-        //}
+    //protected override void OnNewIntent(Intent intent)
+    //{
+    //    Android.Util.Log.Debug(TAG, "Location: OnNewIntent*************");
+    //    base.OnNewIntent(intent);
+    //    PushNotificationManager.ProcessIntent(this, intent);
+    //}
 
-        public bool IsPlayServicesAvailable()
+    public bool IsPlayServicesAvailable()
+    {
+        Android.Util.Log.Debug(TAG, "Location: IsPlayServicesAvailable*************");
+        int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.Success)
         {
-            Android.Util.Log.Debug(TAG, "Location: IsPlayServicesAvailable*************");
-            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
-            if (resultCode != ConnectionResult.Success)
-            {
-                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
-                    Android.Util.Log.Debug(TAG, "GooglePlayServices Connection Error: " + GoogleApiAvailability.Instance.GetErrorString(resultCode));
+            if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                Android.Util.Log.Debug(TAG, "GooglePlayServices Connection Error: " + GoogleApiAvailability.Instance.GetErrorString(resultCode));
 
-                
-                else
-                {
-                    
-                    Android.Util.Log.Debug(TAG, "This device is not supported");
 
-                    Finish();
-                }
-                return false;
-            }
             else
             {
-                
-                Android.Util.Log.Debug(TAG, "Google Play Services is available.");
-                return true;
-            }
-        }
 
-        void CreateNotificationChannel()
+                Android.Util.Log.Debug(TAG, "This device is not supported");
+
+                Finish();
+            }
+            return false;
+        }
+        else
         {
-            Android.Util.Log.Debug(TAG, "Location: CreateNotificationChannel*************");
-            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
-            {
-                // Notification channels are new in API 26 (and not a part of the
-                // support library). There is no need to create a notification
-                // channel on older versions of Android.
-                return;
-            }
 
-            var channel = new NotificationChannel("WootrixV2Channel",
-                                                  "FCM Notifications",
-                                                  NotificationImportance.Default)
-            {
+            Android.Util.Log.Debug(TAG, "Google Play Services is available.");
+            return true;
+        }
+    }
 
-                Description = "Firebase Cloud Messages appear in this channel"
-            };
-
-            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
-            notificationManager.CreateNotificationChannel(channel);
+    void CreateNotificationChannel()
+    {
+        Android.Util.Log.Debug(TAG, "Location: CreateNotificationChannel*************");
+        if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+        {
+            // Notification channels are new in API 26 (and not a part of the
+            // support library). There is no need to create a notification
+            // channel on older versions of Android.
+            return;
         }
 
+        var channel = new NotificationChannel("WootrixV2Channel",
+                                              "FCM Notifications",
+                                              NotificationImportance.Default)
+        {
+
+            Description = "Firebase Cloud Messages appear in this channel"
+        };
+
+        var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+        notificationManager.CreateNotificationChannel(channel);
     }
+
+}
 }
--- WootrixV2Apps/WootrixV2Apps.Android/MainApplication.cs
+++ WootrixV2Apps/WootrixV2Apps.Android/MainApplication.cs
@@ -15,7 +15,6 @@ using Android.Gms.Common;
 using Firebase.Messaging;
 using Firebase.Iid;
 using Android.Util;
-using Plugin.PushNotification;
 
 namespace WootrixV2Apps.Droid
 {
@@ -29,35 +28,6 @@ namespace WootrixV2Apps.Droid
         public override void OnCreate()
         {
             base.OnCreate();
-
-            //If debug you should reset the token each time.
-#if DEBUG
-            PushNotificationManager.Initialize(this, true);
-#else
-            PushNotificationManager.Initialize(this, false);
-#endif
-
-            //Set the default notification channel for your app when running Android Oreo
-            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
-            {
-                //Change for your default notification channel id here
-                PushNotificationManager.DefaultNotificationChannelId = "WootrixV2ChannelID";
-
-                //Change for your default notification channel name here
-                PushNotificationManager.DefaultNotificationChannelName = "WootrixV2ChannelName";
-
-            }
-            //FirebaseMessaging.Instance.SubscribeToTopic("all");
-            //Log.Debug("all", "Subscribed to remote notifications");
-            Console.WriteLine($"Firebase.Current.Token from MainApplication.cs: {CrossPushNotification.Current.Token}");
-            //Console.WriteLine($"FirebaseInstanceId.Instance.Token from from MainApplication.cs: {FirebaseInstanceId.Instance.Token}");
-            //Handle notification when app is closed here
-            CrossPushNotification.Current.OnNotificationReceived += (s, p) =>
-            {
-
-
-            };
-
 
         }
     }
