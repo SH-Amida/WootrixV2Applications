57e059ef6704d1da996c5a9c7e721c44a439ed6b
Sh - Amida <49647116+SH-Amida@users.noreply.github.com>
2019-05-16 15:55:16 -0300

    Back button issue fixed

--- WootrixV2Apps/WootrixV2Apps/MainPage.xaml.cs
+++ WootrixV2Apps/WootrixV2Apps/MainPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using Android.Util;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WootrixV2Apps.Helpers;
 using Xamarin.Forms;
 
 
@@ -11,11 +12,18 @@ using Xamarin.Forms;
 namespace WootrixV2Apps
 {
     public partial class MainPage : ContentPage
-    {        
-
+    {
         public MainPage()
         {
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            backButton.IsVisible = false;
+        }
+
+        protected override void OnAppearing ( ) {
+            base.OnAppearing ( );
+            
+            webView.Navigating += WebView_Navigating;
         }
 
         async void OnBackButtonClicked(object sender, EventArgs e)
@@ -30,16 +38,21 @@ namespace WootrixV2Apps
             }
         }
 
-        void OnForwardButtonClicked(object sender, EventArgs e)
+        void WebView_Navigating ( object sender, WebNavigatingEventArgs e ) {
+            if (!e.Url.StartsWith(ConstantsHelper.BaseUrl)) {
+                backButton.IsVisible = true;
+            } else {
+                backButton.IsVisible = false;
+            }
+        }
+
+        void OnForwardButtonClicked (object sender, EventArgs e)
         {
             if (webView.CanGoForward)
             {
                 webView.GoForward();
             }
         }
-
-       
-
 
     }
 }
